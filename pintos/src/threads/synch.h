f8946f16d8e66317b907885868d634bc0d8a7a2b
yasmimvso <yvso@cin.ufpe.br>
2026-03-29 12:33:23 -0300

    feat(yvso): criação de recalculate_priority para recalcular lock com waiter de maior prioridade na lista de lock

--- pintos/src/threads/synch.h
+++ pintos/src/threads/synch.h
@@ -22,6 +22,7 @@ struct lock
   {
     struct thread *holder;      /* Thread holding lock (for debugging). */
     struct semaphore semaphore; /* Binary semaphore controlling access. */
+    struct list_elem elem /*para que cada lock possa ser um nó na lista `locks_held` de sua thread holder*/
   };
 
 void lock_init (struct lock *);
